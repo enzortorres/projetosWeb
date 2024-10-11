1255aa5da69a27c919912c1f0f62f266e028b1c3
Enzo Ribas <enzortorres2005@gmail.com>
2024-10-10 22:34:13 -0300

    changing parallax speed

--- parallax-website/script.js
+++ parallax-website/script.js
@@ -7,10 +7,10 @@ let hill5 = document.getElementById('hill5')
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 + 'px'
-    leaf.style.top = value * -1.5 + 'px'
-    leaf.style.left = value * 1.5 + 'px'
-    hill5.style.left = value * 1.5 + 'px'
-    hill4.style.left = value * -1.5 + 'px'
+    text.style.marginTop = value * 1.5 + 'px'
+    leaf.style.top = value * -1 + 'px'
+    leaf.style.left = value * 1 + 'px'
+    hill5.style.left = value * 1 + 'px'
+    hill4.style.left = value * -1 + 'px'
     hill1.style.top = value * 1 + 'px'
 })
